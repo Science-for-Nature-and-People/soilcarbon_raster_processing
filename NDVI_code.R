937df99a4c3848aeb8728941bfe4fc9d26a86b8d
csparks <csparks@ucsb.edu>
2018-07-30 16:02:52 -0700

    Finish test script with subsets

--- NDVI_code.R
+++ NDVI_code.R
@@ -6,74 +6,46 @@ library(sp)
 library(leaflet)
 library(tidyverse)
 
-NDVI <- raster("/home/shares/soilcarbon/NDVI_grassland/Raster_Grasslands/CA_Landsat8_maxNDVI_20161101_20170701.tif")how 
-crs(NDVI)
+## Using NDVI and California County datasets as placeholders for subsequent data
 
+## Load data and create subsets to improve processing times
 
-## Issues with Management Zones - have a database/attribute table but no associated geometry.
-## If zones are raster format, they will likely need conversion into shapefile for zonal statistics (this makes most sense to me)
-## For now, will use counties dataset
-
-## Takes too long (never stops hanging) to use all counties, will use subset
+NDVI <- raster("/home/shares/soilcarbon/NDVI_grassland/Raster_Grasslands/CA_Landsat8_maxNDVI_20161101_20170701.tif")
 
 counties <- st_read("/home/shares/soilcarbon/NDVI_grassland/Raster_Grasslands", layer = "CA_Counties_Tiger2016")
 
-counties_test <- counties %>% 
+counties_select <- counties %>% # Don't need the other columns
   select(NAME)
 
-counties_proj <- st_transform(counties_test, "+init=epsg:4326")
-plot(st_geometry(counties_proj), axes = TRUE)
-plot(st_geometry(counties_sub), add = TRUE)
+counties_proj <- st_transform(counties_select, "+init=epsg:4326") # Ensure data is in same projection
 
-## Creating subset
+## Creating subsets - using Mariposa and Ventura counties only
+
 counties_sub <- counties_proj %>% 
   filter(NAME == "Mariposa" | NAME == "Ventura") %>% 
   select(NAME)
 
-plot(NDVI)
-plot(counties_proj["NAME"], add = TRUE)
+NDVI_sub <- mask(NDVI, counties_sub)
 
+## Start analyses
 
- max_number <- raster::extract(NDVI, counties_sub, fun = max, df = TRUE, na.rm = TRUE)
- min_number <- raster::extract(NDVI, counties_sub, fun = min, na.rm = TRUE)
- med_number <- raster::extract(NDVI, counties_sub, fun = median, na.rm = TRUE)
-# max_number <- raster::extract(NDVI, counties_sub, fun = max, sp = TRUE) #Do we want a spatial frame or a df?
- ## It works but takes a very very long time
+## Functions to get the max, min, and median values of the NDVI subset for the county subset
 
+max_number <- raster::extract(NDVI_sub, counties_sub, fun = max, na.rm = TRUE)
+min_number <- raster::extract(NDVI_sub, counties_sub, fun = min, na.rm = TRUE)
+med_number <- raster::extract(NDVI_sub, counties_sub, fun = median, na.rm = TRUE)
 
+## Accesses the resulting matrices and appends the relevant column to the counties shapefile for display if needed
 
+NDVI_sub_stats <- counties_sub %>% 
+  mutate(max_number = max_number[,1]) %>% 
+  mutate(min_number = min_number[,1]) %>% 
+  mutate(med_number = med_number[,1])
 
-# ####
-# #import required libraries
-# library(maptools)
-# library(raster)
-# 
-# #list files (in this case raster TIFFs)
-# grids <- list.files("./path/to/data", pattern = "*.tif$")
-# 
-# #check the number of files in the raster list (grids)
-# length <- length(grids)
-# 
-# #read-in the polygon shapefile
-# poly <- readShapePoly("./path/to/data/shapefile.shp")
-# 
-# #create a raster stack
-# s <- stack(paste0("./path/to/data/", grids))
-# 
-# #extract raster cell count (sum) within each polygon area (poly)
-# for (i in 1:length(grids)){
-#   ex <- extract(s, poly, fun=sum, na.rm=TRUE, df=TRUE)
-# }
-# 
-# #write to a data frame
-# df <- data.frame(ex)
-# 
-# #write to a CSV file
-# write.csv(df, file = "./path/to/data/CSV.csv")
-# ###
+## Converts shapefile into csv file format
 
+NDVI_sub_stats_df <- NDVI_sub_stats %>% # Create dataframe out of file to remove geometry
+  st_set_geometry(NULL)
 
-
-#grazing_zones <- raster("/home/shares/soilcarbon/NDVI_grassland/Raster_Grasslands/RMZ/RMZ.tif")
-
-#grazing_zones <- st_read(system.file("/home/shares/soilcarbon/NDVI_grassland/Raster_Grasslands", layer = "RMZ"))
+path_for_csv <- getwd()
+write.csv(NDVI_sub_stats_df, file = file.path(path_for_csv, "NDVI_subset_statistics.csv"), row.names = FALSE)
