533937a3fd2418903f22758f74d03a0f15f1f321
csparks <csparks@ucsb.edu>
2018-07-31 16:58:03 -0700

    minor edits

--- NDVI_code.R
+++ NDVI_code.R
@@ -3,23 +3,24 @@ setwd("/home/csparks/soilcarbon_raster_processing")
 library(raster)
 library(sf)
 library(sp)
-library(leaflet)
 library(tidyverse)
 
 ## Using NDVI and California County datasets as placeholders for subsequent data
 
-## Load data and create subsets to improve processing times
+## Load data
 
 NDVI <- raster("/home/shares/soilcarbon/NDVI_grassland/Raster_Grasslands/CA_Landsat8_maxNDVI_20161101_20170701.tif")
 
 counties <- st_read("/home/shares/soilcarbon/NDVI_grassland/Raster_Grasslands", layer = "CA_Counties_Tiger2016")
 
-counties_select <- counties %>% # Don't need the other columns
+## Data wrangling
+
+counties_select <- counties %>% # Drop all columns except name
   select(NAME)
 
 counties_proj <- st_transform(counties_select, "+init=epsg:4326") # Ensure data is in same projection
 
-## Creating subsets - using Mariposa and Ventura counties only
+## Creating subsets - using Mariposa and Ventura counties only, crop NDVI to this extent
 
 counties_sub <- counties_proj %>% 
   filter(NAME == "Mariposa" | NAME == "Ventura") %>% 
@@ -29,7 +30,7 @@ NDVI_sub <- mask(NDVI, counties_sub)
 
 ## Start analyses
 
-## Functions to get the max, min, and median values of the NDVI subset for the county subset
+## Functions to get the max, min, and median values of the NDVI subset for the county subset. Creates matrix.
 
 max_number <- raster::extract(NDVI_sub, counties_sub, fun = max, na.rm = TRUE)
 min_number <- raster::extract(NDVI_sub, counties_sub, fun = min, na.rm = TRUE)
@@ -46,6 +47,9 @@ NDVI_sub_stats <- counties_sub %>%
 
 NDVI_sub_stats_df <- NDVI_sub_stats %>% # Create dataframe out of file to remove geometry
   st_set_geometry(NULL)
+
+## Alternate way to drop geometry (but need to know what columns to select)
+# NDVI_sub_stats_df2 <- NDVI_sub_stats[, c(1:4), drop = TRUE]
 
 path_for_csv <- getwd()
 write.csv(NDVI_sub_stats_df, file = file.path(path_for_csv, "NDVI_subset_statistics.csv"), row.names = FALSE)
