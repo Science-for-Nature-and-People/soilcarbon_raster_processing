57a7f304d5c86dc10150ddc7704885ba111edf38
csparks <csparks@ucsb.edu>
2018-08-07 15:15:48 -0700

    Add write raster

--- NDVI_code.R
+++ NDVI_code.R
@@ -1,4 +1,5 @@
 setwd("/home/csparks/soilcarbon_raster_processing")
+path_for_csv <- getwd()
 
 library(raster)
 library(sf)
@@ -15,7 +16,9 @@ rangelands <- raster("/home/shares/soilcarbon/soilc-california/rangeland-product
 
 ## Data wrangling
 
-# Define the projection to use (ESPG 3310: NAD 83 California Albers)
+## Define the projection to use (ESPG 3310: NAD 83 California Albers)
+## Takes a long time to process
+
 newproj <- "+proj=aea +lat_1=34 +lat_2=40.5 +lat_0=0 +lon_0=-120 +x_0=0 +y_0=-4000000 +ellps=GRS80 +datum=NAD83 +units=m +no_defs" 
 
 proj <- projectExtent(rangelands, crs = newproj)  # Reproject the RMZ file
@@ -34,6 +37,9 @@ med_raster <- subs(range_proj, med_NDVI_df, by=1, which=2, subsWithNA = TRUE) # 
 
 ratio_raster <- NDVI_proj/med_raster # Values less than one mean that the NDVI value at that cell is less than the median for that RMZ type
 
+## Write final raster to file
+
+writeRaster(ratio_raster, filename = "RMZ_NDVI_Median_Ratio", format = "GTiff")
 
 ## CROPPED VERSION
 
@@ -52,7 +58,7 @@ range_crop <- raster("/home/shares/soilcarbon/NDVI_grassland/Raster_Grasslands/R
 crs(range_crop) <- crs(rangelands)
 
 # Define the projection to use (ESPG 3310: NAD 83 California Albers)
-newproj <- "+proj=aea +lat_1=34 +lat_2=40.5 +lat_0=0 +lon_0=-120 +x_0=0 +y_0=-4000000 +ellps=GRS80 +datum=NAD83 +units=m +no_defs" 
+newproj <- "+proj=aea +lat_1=34 +lat_2=40.5 +lat_0=0 +lon_0=-120 +x_0=0 +y_0=-4000000 +ellps=GRS80 +datum=NAD83 +units=m +no_defs"
 
 proj <- projectExtent(range_crop, crs = newproj)  # Reproject the RMZ file
 range_crop_proj <- projectRaster(range_crop, proj, method= "ngb", alignOnly = FALSE)
@@ -70,49 +76,6 @@ med_raster <- subs(range_crop_proj, med_NDVI_df, by=1, which=2, subsWithNA = TRU
 
 ratio_raster <- NDVI_proj/med_raster # Values less than one mean that the NDVI value at that cell is less than the median for that RMZ type
 
+## Write final raster to file
 
-
-#### OLD CODE
-
-#counties <- st_read("/home/shares/soilcarbon/NDVI_grassland/Raster_Grasslands", layer = "CA_Counties_Tiger2016")
-
-#range_poly <- rasterToPolygons(rangelands, n = 16, na.rm =TRUE, digits = 12, dissolve = TRUE)
-
-#counties_select <- counties %>% # Drop all columns except name
-#  select(NAME)
-
-#counties_proj <- st_transform(counties_select, "+init=epsg:4326") # Ensure data is in same projection
-
-## Creating subsets - using Mariposa and Ventura counties only, crop NDVI to this extent
-
-#counties_sub <- counties_proj %>% 
-#  filter(NAME == "Mariposa" | NAME == "Ventura") %>% 
-#  select(NAME)
-
-#NDVI_sub <- mask(NDVI, counties_sub)
-
-## Start analyses
-
-## Functions to get the max, min, and median values of the NDVI subset for the county subset. Creates matrix.
-
-# max_number <- raster::extract(NDVI_sub, counties_sub, fun = max, na.rm = TRUE)
-# min_number <- raster::extract(NDVI_sub, counties_sub, fun = min, na.rm = TRUE)
-# med_number <- raster::extract(NDVI_sub, counties_sub, fun = median, na.rm = TRUE)
-
-## Accesses the resulting matrices and appends the relevant column to the counties shapefile for display if needed
-
-# NDVI_sub_stats <- counties_sub %>% 
-#   mutate(max_number = max_number[,1]) %>% 
-#   mutate(min_number = min_number[,1]) %>% 
-#   mutate(med_number = med_number[,1])
-# 
-# ## Converts shapefile into csv file format
-# 
-# NDVI_sub_stats_df <- NDVI_sub_stats %>% # Create dataframe out of file to remove geometry
-#   st_set_geometry(NULL)
-# 
-# ## Alternate way to drop geometry (but need to know what columns to select)
-# # NDVI_sub_stats_df2 <- NDVI_sub_stats[, c(1:4), drop = TRUE]
-# 
-# path_for_csv <- getwd()
-# write.csv(NDVI_sub_stats_df, file = file.path(path_for_csv, "NDVI_subset_statistics.csv"), row.names = FALSE)
+writeRaster(ratio_raster, filename = "RMZ_NDVI_Median_Ratio_CROP3.tif", format = "GTiff", overwrite = TRUE, datatype = "FLT4S")
